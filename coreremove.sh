1af237c233d211dc6462937d34ebc4566e54c398
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-04-25 03:33:42 -0600

    Minor fixes

--- coreremove.sh
+++ coreremove.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #Core remover
 
-logdir = "/var/log/maui"
+logdir="/var/log/maui"
 echo "Checking log directory..."
-pretotal = $(ls -l $logdir | wc -l)
-echo "There are $pretotal logs in the //maui directory."
-echo "Deleting files"
+pretotal=$(ls -l $logdir | wc -l)
+echo "There are $pretotal logs in the $logdir directory."
+echo "Deleting files..."
 #rm -rf $logdir
-posttotal = $(ls -l $logdir | wc -l)
-diff = $pretotal - $posttotal
-echo "There are $posttotal logs in the //maui directory."
+posttotal=$(ls -l $logdir | wc -l)
+diff = $(pretotal - posttotal)
+echo "There are $posttotal logs in the $logdir directory."
 echo "$diff logs were deleted."
 exit 0
