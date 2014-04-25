d3dfd00394d9879d34cec0b6568a965557d85c70
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-04-25 06:04:53 -0600

    minor fix

--- coreremove.sh
+++ coreremove.sh
@@ -8,7 +8,7 @@ echo "There are $pretotal logs in the $logdir directory."
 echo "Deleting files..."
 #rm -rf $logdir
 posttotal=$(ls -l $logdir | wc -l)
-diff = $(pretotal - posttotal)
+diff=$(pretotal - posttotal)
 echo "There are $posttotal logs in the $logdir directory."
 echo "$diff logs were deleted."
 exit 0
