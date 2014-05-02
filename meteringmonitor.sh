8577f72a4edeccde85bfa75041808fe6eda443e7
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-05-02 06:48:09 -0600

    Additional refactoring, added new logic to alert

--- meteringmonitor.sh
+++ meteringmonitor.sh
@@ -2,11 +2,13 @@
 # Metering Log Monitor
 # Written by Daniel Kolkena
 
-version=1.3.1
+version=1.3.2
+
+MAPLOG="/opt/cloudcommon/metering/logs/map_reduce_output.log"
 
 newprev=$(hadoop fs -ls /meteringlogs/new/ | wc -l)
 procprev=$(hadoop fs -ls /meteringlogs/processing/ | wc -l)
-logtailprev=$(tail -1 /opt/cloudcommon/metering/logs/map_reduce_output.log | cut -c -80)
+logtailprev=$(tail -1 $MAPLOG | cut -c -80)
 
 i=0 		# Iteration counter
 t="5m" 		# Refresh increment: s=seconds, m=minutes, h=hours
@@ -47,14 +49,15 @@ function showLogTail()
 {
 	printf "\nCurrent tail of map_reduce_output.log:\n"
 	printf "================================================================================\n"
-	tail -5 /opt/cloudcommon/metering/logs/map_reduce_output.log | cut -c -80
+	tail -5 $MAPLOG | cut -c -80
 	printf "================================================================================\n"
 }
 
 function showAlert() 
 {
 	pids=$(for pid in $(ps -e -o pid= -o stat= -o comm= -o args | grep mapreduce | grep -v grep | awk '{print $1}'); do printf "$pid "; done;)
-	printf "\nAlert! The number of logs in /new has not gone down in 1 hour."
+	printf "\nAlert! The number of logs in /new has not gone down in 1 hour,"
+	printf "\nand the map_reduce_output.log hasn't moved in that time either."
 	printf "\nConsider running \"kill -9 $pids\b\" on the metering node.\n\n"
 }
 
@@ -67,6 +70,7 @@ while [[ $i -le $max ]] # Default: will run a max of 144 iterations of 5 minutes
 do	
 	new=$(hadoop fs -ls /meteringlogs/new/ | wc -l)
 	proc=$(hadoop fs -ls /meteringlogs/processing/ | wc -l)
+	logtail=$(tail -1 $MAPLOG | cut -c -80)
 
 	clear
 
@@ -76,9 +80,9 @@ do
 	showLogTail
 
 	# Alert timer counter
-	if [[ $((new-newprev)) -lt 0 ]]
+	if [[ $((new-newprev)) -lt 0 ]] && [[ "$logtailprev" != "$logtail" ]]
 		then 
-			a=0					# If /new falls, reset counter
+			a=0					# If /new falls or the map_reduce_log has changed, reset counter
 		else
 			a=$(( a + 1 ))		# Increment counter
 	fi
@@ -91,7 +95,7 @@ do
 	echo "[Hit CTRL+C to end]"
 
 	# Update loop values
-	writeToLog; newprev=$new; procprev=$proc; i=$(( i + 1 ));
+	writeToLog; newprev=$new; procprev=$proc; logtailprev=$logtail; i=$(( i + 1 ));
 
 	sleep $t 				# Default: refreshes every 5 minutes.
 done
