d867771dfc217d0ef89d4a706d045d9d35d69c66
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-05-02 02:25:16 -0600

    Major refactor into functions

--- meteringmonitor.sh
+++ meteringmonitor.sh
@@ -2,7 +2,7 @@
 # Metering Log Monitor
 # Written by Daniel Kolkena
 
-version=1.2.1
+version=1.3
 
 newprev=$(hadoop fs -ls /meteringlogs/new/ | wc -l)
 procprev=$(hadoop fs -ls /meteringlogs/processing/ | wc -l)
@@ -12,8 +12,7 @@ max=144 	# Number of loops before the script ends
 a=0			# Alert counter
 amax=12		# Number of loops before alert procs 
 
-while [[ $i -le $max ]] # Default: will run a max of 144 iterations of 5 minutes, or 12 hours
-do	
+function showLogCounts() {
 	new=$(hadoop fs -ls /meteringlogs/new/ | wc -l)
 	proc=$(hadoop fs -ls /meteringlogs/processing/ | wc -l)
 
@@ -22,11 +21,15 @@ do
 	printf "Metering Logs:\n"
 	printf "New:\t\t%d\t(%+d)\n" "$new" "$((new-newprev))"
 	printf "Processing:\t%d\t(%+d)\n\n" "$proc" "$((proc-procprev))"
+}
 
+function showNodeInfo() {
 	/opt/cloudcommon/flume/bin/flume shell -c localhost -e getnodestatus | grep -v ACTIVE
 	hadoop dfsadmin -report | grep Datanodes 
 	hadoop dfsadmin -report | grep Decommission
-	
+}
+
+function showMapreduceProcesses() {
 	printf "\nCurrent Mapreduce processes:\n"
 	printf "================================================================================\n"
 	ps -e -o pid= -o comm= -o args | grep mapreduce | grep -v grep | cut -c -80
@@ -37,11 +40,31 @@ do
 	# 		printf "$pid\t"; cat /proc/$pid/status | grep State
 	# 	done					# Commented out until I can figure out what fields are useful in /proc/*/status
 	printf "================================================================================\n"
+}
 
+function showLogTail() {
 	printf "\nCurrent tail of map_reduce_output.log:\n"
 	printf "================================================================================\n"
 	tail -5 /opt/cloudcommon/metering/logs/map_reduce_output.log | cut -c -80
 	printf "================================================================================\n"
+}
+
+function showAlert() {
+	pids=$(for pid in $(ps -e -o pid= -o stat= -o comm= -o args | grep mapreduce | grep -v grep | awk '{print $1}'); do printf "$pid "; done;)
+	printf "\nAlert! The number of logs in /new has not gone down in 1 hour."
+	printf "\nConsider running \"kill -9 $pids\b\" on the metering node.\n\n"
+}
+
+function writeToLog() {
+	echo "$(date): " $new >> monitor.log # Writing output to logfile
+}
+
+while [[ $i -le $max ]] # Default: will run a max of 144 iterations of 5 minutes, or 12 hours
+do	
+	showLogCounts
+	showNodeInfo
+	showMapreduceProcesses
+	showLogTail
 
 	# Alert timer counter
 	if [[ $((new-newprev)) -lt 0 ]]
@@ -53,19 +76,15 @@ do
 
 	if [[ $a -ge $amax ]] 	# If /new isn't falling for 1 hour, a warning will show
 	then
-		pids=$(for pid in $(ps -e -o pid= -o stat= -o comm= -o args | grep mapreduce | grep -v grep | awk '{print $1}'); do printf "$pid "; done;)
-		printf "\nAlert! The number of logs in /new has not gone down in 1 hour."
-		printf "\nConsider running \"kill -9 $pids\b\" on the metering node.\n\n"
+		showAlert
 	fi
-
 	echo "[Hit CTRL+C to end]"
 
-	echo "$(date): " $new >> monitor.log # Writing output to logfile
-
+	writeToLog
 	newprev=$new
 	procprev=$proc
 	i=$(( i + 1 ))
-	sleep $t # Default: refreshes every 5 minutes.
+	sleep $t 				# Default: refreshes every 5 minutes.
 done
 
 echo "Exiting after 12 hours."
