e22a1d0ff4a57c06bfec69dccc0304fb63e38e08
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-05-09 21:58:02 -0600

    Fixed variable bug and if logic

--- ssreplace.sh
+++ ssreplace.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 # SS Disk Proactive Replacement Script
 
-version=0.1 # First Draft, commented out for testing purposes
+version=0.2 # Second Draft, commented out for testing purposes
 
 # Data Input
 echo "SS Disk Replacement Script"
-read -p "Target Host name?: " TARGET_HOST_NAME
-read -p "FSUUID?: " FSUUID
+read -p "Target Host name?: " target_host_name
+read -p "FSUUID?: " fsuuid
 echo ""
 
 # Finding other variables
-VERSION=$(rpm -qa atmos | cut -c 7-11)
-MASTER=$(ssh $TARGET_HOST_NAME show_master.py | grep System | awk -F " " '{print $3}')
-PATH=$(df -h | grep $FSUUID | awk '{print $1}')
-DISKIDX=$(ls -l /var/local/maui/atmos-diskman/INDEX/ | grep $FSUUID | cut -d : -f2 | cut -d . -f1)
-NODEUUID=$(psql -U postgres rmg.db -h $MASTER -c "select uuid from nodes where hostname='$TARGET_HOST_NAME'" | awk 'NR==3' | cut -c 2-37)
+atversion=$(rpm -qa atmos | cut -c 7-11)
+master=$(ssh $target_host_name show_master.py | grep System | awk -F " " '{print $3}')
+path=$(df -h | grep $fsuuid | awk '{print $1}')
+diskidx=$(ls -l /var/local/maui/atmos-diskman/INDEX/ | grep $fsuuid | cut -d : -f2 | cut -d . -f1)
+nodeuuid=$(psql -U postgres rmg.db -h $master -c "select uuid from nodes where hostname='$target_host_name'" | awk 'NR==3' | cut -c 2-37)
 
 # Verification Step
-echo "Atmos version is: $VERSION"
-echo "Host node is: $TARGET_HOST_NAME"
-echo "Master node is: $MASTER"
-echo "Disk Path is: $PATH"
-echo "Disk FSUUID is: $FSUUID"
-echo "Disk ID is: $DISKIDX"
-echo "Node UUID is: $NODEUUID"
+echo "Atmos version is: $atversion"
+echo "Host node is: $target_host_name"
+echo "Master node is: $master"
+echo "Disk Path is: $path"
+echo "Disk FSUUID is: $fsuuid"
+echo "Disk ID is: $diskidx"
+echo "Node UUID is: $nodeuuid"
 echo ""
-read -p "Does this information look correct? (y/n): " ANS
+read -p "Does this information look correct? (y/n): " ans
 
-if [[ $ANS == "y" || "Y" ]];
+if [[ $ans = [Yy] ]];
 	then
 	echo "Proceeding with disk replacement."
 	# Here's where the magic happens
-	# mauirexec "mauisvcmgr -s mauicc -c trigger_cc_rcvrtask -a 'queryStr=\"DISKID-$TARGET_HOST_NAME:$DISKIDX\",act=ConsCheck,taskId=$TARGET_HOST_NAME:$DISKIDX'" 
-	# mauirexec "mauisvcmgr -s mauicc -c query_cc_rcvrtask -a 'taskId=$TARGET_HOST_NAME:$DISKIDX' | grep status="
-	# psql -U postgres rmg.db -h $MASTER -c "select uuid from disks where nodeuuid='$NODEUUID' and devpath='$PATH'"
-elif [[ $ANS == "n" || "Y" ]];
+	# mauirexec "mauisvcmgr -s mauicc -c trigger_cc_rcvrtask -a 'queryStr=\"DISKID-$target_host_name:$diskidx\",act=ConsCheck,taskId=$target_host_name:$diskidx'" 
+	# mauirexec "mauisvcmgr -s mauicc -c query_cc_rcvrtask -a 'taskId=$target_host_name:$diskidx' | grep status="
+	# psql -U postgres rmg.db -h $MASTER -c "select uuid from disks where nodeuuid='$nodeuuid' and devpath='$path'"
+elif [[ $ans = [Nn] ]];
 	then
 	echo "Disk replacement cancelled."
 else
