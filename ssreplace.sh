b8a3fcc71f22abde966d3254720b46c04a99d7a7
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-05-12 04:44:16 -0600

    Almost finished, just needs verification step

--- ssreplace.sh
+++ ssreplace.sh
@@ -2,7 +2,7 @@
 # Written by Daniel Kolkena, based on work from Bret Miller
 # SS Disk Proactive Replacement Script
 
-version=0.5 # Draft, commented out for testing purposes
+version=0.9 # Draft, commented out for testing purposes
 
 validuuid='^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$' # Regex to confirm a valid UUID
 
@@ -18,7 +18,7 @@ echo
 atversion=$(rpm -qa atmos | cut -c 7-11)
 master=$(ssh "$target_host_name" show_master.py | grep System | awk -F " " '{print $3}')
 path=$(df -h | grep "$fsuuid" | awk '{print $1}')
-diskidx=$(ls /var/local/maui/atmos-diskman/INDEX/ | grep "$fsuuid" | cut -d : -f2 | cut -d . -f1)
+diskidx=$(cat /etc/fstab | grep mauiss | nl | grep "$fsuuid" | awk '{print $1}')
 nodeuuid=$(psql -U postgres rmg.db -h "$master" -c "select uuid from nodes where hostname='"$target_host_name"'" | awk 'NR==3' | cut -c 2-37)
 
 # Verification Step
@@ -34,21 +34,23 @@ read -p "Does this information look correct? (y/n): " ans
 
 if [[ $ans = [Yy] ]];
 	then
-	echo "Proceeding with disk replacement."
+	echo "Proceeding with disk replacement..."
+	
 	# Here's where the magic happens
-	# TESTING MODE: DISPLAYING COMMANDS ONLY
-	echo "mauirexec \"mauisvcmgr -s mauicc -c trigger_cc_rcvrtask -a 'queryStr=\"DISKID-$target_host_name:$diskidx\",act=ConsCheck,taskId=$target_host_name:$diskidx'\" "
-	echo "mauirexec \"mauisvcmgr -s mauicc -c query_cc_rcvrtask -a 'taskId=$target_host_name:$diskidx' | grep status=\" "
-	echo "psql -U postgres rmg.db -h $master -c \"select uuid from disks where nodeuuid='$nodeuuid' and devpath='$path'\" "
+	mauirexec "mauisvcmgr -s mauicc -c trigger_cc_rcvrtask -a 'queryStr=\"DISKID-$target_host_name:$diskidx\",act=ConsCheck,taskId=$target_host_name:$diskidx'"
+	mauirexec "mauisvcmgr -s mauicc -c query_cc_rcvrtask -a 'taskId=$target_host_name:$diskidx' | grep status="
+	psql -U postgres rmg.db -h $master -c "select uuid from disks where nodeuuid='$nodeuuid' and devpath='$path'"
 	
 	# Validate success here
-	# if invalid; echo "Disk replacement unsuccessful"; exit 1; 
-	# else echo "Proactive disk replacement begun."		# To do: Test disk replacement logic, add error handling for unsuccessful replacement
+	# Test success
+	# if !success; echo "Disk replacement unsuccessful"; exit 1; else
+	echo "Proactive disk replacement begun successfully."		# To do: add error handling for unsuccessful replacement
 	# fi
-	
+
 elif [[ $ans = [Nn] ]];
 	then
 	echo "Disk replacement cancelled."
+	exit 0
 else
 	echo "Response was not 'y' or 'n'. Exiting."
 	exit 1
