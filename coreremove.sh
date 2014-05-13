415c14d5ebecbc1a1bbed5e24f10be4db884148a
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-05-13 00:46:25 -0600

    Added find

--- coreremove.sh
+++ coreremove.sh
@@ -2,13 +2,14 @@
 #Core remover
 
 logdir="/var/log/maui"
+
 echo "Checking log directory..."
 pretotal=$(ls -l $logdir | wc -l)
 echo "There are $pretotal logs in the $logdir directory."
+
 echo "Deleting files..."
-#rm -rf $logdir
-posttotal=$(ls -l $logdir | wc -l)
-diff=$(pretotal - posttotal)
-echo "There are $posttotal logs in the $logdir directory."
-echo "$diff logs were deleted."
+# find $logdir -mindepth 2 -name "*core*" -size +10M -type f -delete # Find all files in $logdir with name "*core*" greater than 10 MB and delete all files recursively.
+
+posttotal=$(ls -l $logdir | wc -l); diff=$(pretotal - posttotal)
+echo "There are $posttotal logs in the $logdir directory. $diff logs were deleted."
 exit 0
