1752b53f57099c262a757781912918e61966a7df
Daniel Kolkena <daniel.kolkena@gmail.com>
2014-06-28 05:31:08 -0600

    Finished script snippet to clear cores

--- coreremove.sh
+++ coreremove.sh
@@ -1,14 +1,14 @@
+#!/bin/bash
 
+echo; 
+corenum=$(find /var/cores -type f | wc -l); echo "Core count: $corenum"; 
+echo "/var/cores is $(du -sh /var/cores | awk -F " " '{print $1}')"; 
 
-logdir="/var/log/maui"
+echo "Deleting cores..."; find /var/cores -type f -mtime +2 -delete; 
 
-echo "Checking log directory..."
-pretotal=$(ls -l $logdir | wc -l)
-echo "There are $pretotal logs in the $logdir directory."
+newcorenum=$(find /var/cores -type f | wc -l); echo "Core count: $newcorenum"; 
+echo "/var/cores is $(du -sh /var/cores | awk -F " " '{print $1}')"; 
 
-echo "Deleting files..."
-# find $logdir -mindepth 2 -name "*core*" -size +10M -type f -delete # Find all files in $logdir with name "*core*" greater than 10 MB and delete all files recursively.
-
-posttotal=$(ls -l $logdir | wc -l); diff=$(pretotal - posttotal)
-echo "There are $posttotal logs in the $logdir directory. $diff logs were deleted."
-exit 0
+echo "$(( corenum - newcorenum )) cores deleted."; 
+unset corenum; unset newcorenum; 
+exit
